d040ec37a730f5d539e42fbf3201045327ef4d5a
sj7699 <tkdwo7699@gmail.com>
2022-07-01 23:08:19 +0900

    java 버전 수정

--- Backend/src/main/java/kau/isabu/web_project/WebProjectApplication.java
+++ Backend/src/main/java/kau/isabu/web_project/WebProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 // 컴파일 확인을 위한 설정 -> DB 설정 시 제거할 것
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class WebProjectApplication {
 
     public static void main(String[] args) {
